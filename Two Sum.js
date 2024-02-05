e7e016e3235fa3976606fb32ab4c95488517d805
Grootoportunities <tapir1337@mail.ru>
2024-02-05 14:11:44 +0300

    Two Sum

---
+++ Two Sum.js
@@ -0,0 +1,22 @@
+function twoSum(numbers, target) {
+    let indexArray = []
+
+    for (let i = 0; i < numbers.length; i++) {
+
+        if (indexArray.length !== 0) break
+        else {
+
+            for (let j = 1; j < numbers.length; j++) {
+                if (numbers[i] + numbers[j] === target) {
+                    indexArray.push(i)
+                    indexArray.push(j)
+                    break
+                }
+            }
+        }
+    }
+
+    return indexArray
+}
+
+console.log(twoSum([2, 2, 3], 4))
